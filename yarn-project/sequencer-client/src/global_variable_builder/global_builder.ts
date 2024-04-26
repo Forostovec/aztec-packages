noir-lang/noir

commit 82b17c8f0e9207db803fd3b824e63bac25ea69f6

feat: Validate globals in public kernel (#6031)
- Adds a new `Globals` to `PublicCircuitPublicInputs`, which just
forwards the globals injected into the public call via the
`PublicContextInputs`.
- Adds a new `Globals` to the kernel's `CombinedConstantData`. This is
zero in private-land, and manually set to whatever globals the sequencer
determines in public-land.
- Validates these globals match between kernel and public call in public
kernels, and that they match the ones in the base rollup circuit.
- Loads block gas fees from the `CombinedConstantData` in the public
kernel to calculate the tx fee.

Fixes https://github.com/AztecProtocol/aztec-packages/issues/5843

noir-projects/noir-protocol-circuits/crates/public-kernel-lib/src/public_kernel_teardown.nr
@@ -36,8 +36,7 @@ impl PublicKernelTeardownCircuitPrivateInputs {
         let transaction_fee = self.public_call.call_stack_item.public_inputs.transaction_fee;
         // Note that teardown_gas is already included in end.gas_used as it was injected by the private kernel
         let total_gas_used = self.previous_kernel.public_inputs.end.gas_used.add(self.previous_kernel.public_inputs.end_non_revertible.gas_used);
-        // TODO(palla/gas): Load gas fees from a PublicConstantData struct that's currently missing
-        let block_gas_fees = GasFees::default();
+        let block_gas_fees = self.previous_kernel.public_inputs.constants.global_variables.gas_fees;
         let inclusion_fee = self.previous_kernel.public_inputs.constants.tx_context.gas_settings.inclusion_fee;
         let computed_transaction_fee = total_gas_used.compute_fee(block_gas_fees) + inclusion_fee;
 
@@ -73,6 +72,9 @@ impl PublicKernelTeardownCircuitPrivateInputs {
 
         // validate the inputs common to all invocation circumstances
         common::validate_inputs(self.public_call);
+
+        // validate constants injected into the public call are correct or set them if this is the first public call
+        common::initialize_from_or_validate_public_call_variables(self.previous_kernel, self.public_call, &mut public_inputs);
 
         // validate the inputs unique to having a previous private kernel
         self.validate_inputs();
@@ -422,6 +424,27 @@ mod tests {
     fn validates_transaction_fee() {
         let mut builder = PublicKernelTeardownCircuitPrivateInputsBuilder::new();
         builder.public_call.public_inputs.transaction_fee = 1234;
+
+        builder.failed();
+    }
+
+    #[test]
+    fn propagates_global_variables_if_empty() {
+        let mut builder = PublicKernelTeardownCircuitPrivateInputsBuilder::new();
+
+        builder.public_call.public_inputs.global_variables.block_number = 11;
+
+        let public_inputs = builder.execute();
+
+        assert_eq(public_inputs.constants.global_variables.block_number, 11);
+    }
+
+    #[test(should_fail_with="Global variables injected into the public call do not match constants")]
+    fn validates_global_variables() {
+        let mut builder = PublicKernelTeardownCircuitPrivateInputsBuilder::new();
+
+        builder.previous_kernel.global_variables.block_number = 10;
+        builder.public_call.public_inputs.global_variables.block_number = 11;
 
         builder.failed();
     }

l1-contracts/src/core/libraries/ConstantsGen.sol
@@ -144,8 +144,9 @@ library Constants {
     + MAX_PUBLIC_CALL_STACK_LENGTH_PER_CALL + (SIDE_EFFECT_LENGTH * MAX_NEW_NOTE_HASHES_PER_CALL)
     + (SIDE_EFFECT_LINKED_TO_NOTE_HASH_LENGTH * MAX_NEW_NULLIFIERS_PER_CALL)
     + (L2_TO_L1_MESSAGE_LENGTH * MAX_NEW_L2_TO_L1_MSGS_PER_CALL) + 2
-    + (SIDE_EFFECT_LENGTH * MAX_UNENCRYPTED_LOGS_PER_CALL) + 1 + HEADER_LENGTH + AZTEC_ADDRESS_LENGTH /* revert_code */
-    + 1 + 2 * GAS_LENGTH /* transaction_fee */ + 1;
+    + (SIDE_EFFECT_LENGTH * MAX_UNENCRYPTED_LOGS_PER_CALL) + 1 + HEADER_LENGTH
+    + GLOBAL_VARIABLES_LENGTH + AZTEC_ADDRESS_LENGTH /* revert_code */ + 1 + 2 * GAS_LENGTH /* transaction_fee */
+    + 1;
   uint256 internal constant PRIVATE_CALL_STACK_ITEM_LENGTH =
     AZTEC_ADDRESS_LENGTH + FUNCTION_DATA_LENGTH + PRIVATE_CIRCUIT_PUBLIC_INPUTS_LENGTH;
   uint256 internal constant ENQUEUE_PUBLIC_FUNCTION_CALL_RETURN_LENGTH =

noir-projects/aztec-nr/aztec/src/context/private_context.nr
@@ -9,7 +9,7 @@ use crate::{
 };
 use dep::protocol_types::{
     abis::{
-    gas::Gas, call_context::CallContext, function_data::FunctionData,
+    global_variables::GlobalVariables, gas::Gas, call_context::CallContext, function_data::FunctionData,
     function_selector::FunctionSelector, max_block_number::MaxBlockNumber,
     nullifier_key_validation_request::NullifierKeyValidationRequest,
     private_call_stack_item::PrivateCallStackItem,
@@ -500,6 +500,7 @@ impl PrivateContext {
                 unencrypted_logs_hashes: [SideEffect::empty(); MAX_UNENCRYPTED_LOGS_PER_CALL],
                 unencrypted_log_preimages_length: 0,
                 historical_header: Header::empty(),
+                global_variables: GlobalVariables::empty(),
                 prover_address: AztecAddress::zero(),
                 revert_code: 0,
                 start_gas_left: Gas::empty(),

noir-projects/aztec-nr/aztec/src/context/public_context.nr
@@ -167,6 +167,7 @@ impl PublicContext {
             unencrypted_logs_hashes: self.unencrypted_logs_hashes.storage,
             unencrypted_log_preimages_length,
             historical_header: self.inputs.historical_header,
+            global_variables: self.inputs.public_global_variables,
             prover_address: self.prover_address,
             revert_code: 0,
             start_gas_left: self.inputs.gas_left,

noir-projects/noir-protocol-circuits/crates/private-kernel-lib/src/private_kernel_init.nr
@@ -19,10 +19,10 @@ struct PrivateKernelInitCircuitPrivateInputs {
 
 impl PrivateKernelInitCircuitPrivateInputs {
     fn initialize_end_values(self, public_inputs: &mut PrivateKernelCircuitPublicInputsBuilder) {
-        public_inputs.constants = CombinedConstantData {
-            historical_header: self.private_call.call_stack_item.public_inputs.historical_header,
-            tx_context: self.tx_request.tx_context,
-        };
+        public_inputs.constants = CombinedConstantData::private(
+            self.private_call.call_stack_item.public_inputs.historical_header,
+            self.tx_request.tx_context,
+        );
         public_inputs.min_revertible_side_effect_counter = self.private_call.call_stack_item.public_inputs.min_revertible_side_effect_counter;
     }
 

noir-projects/noir-protocol-circuits/crates/public-kernel-lib/src/common.nr
@@ -4,7 +4,8 @@ use dep::types::{
     kernel_circuit_public_inputs::PublicKernelCircuitPublicInputsBuilder, kernel_data::PublicKernelData,
     public_call_data::PublicCallData, public_data_read::PublicDataRead,
     public_data_update_request::PublicDataUpdateRequest, read_request::ReadRequestContext,
-    side_effect::{SideEffect, SideEffectLinkedToNoteHash}
+    side_effect::{SideEffect, SideEffectLinkedToNoteHash}, global_variables::GlobalVariables,
+    combined_constant_data::CombinedConstantData
 },
     address::AztecAddress,
     contrakt::{storage_read::StorageRead, storage_update_request::StorageUpdateRequest},
@@ -31,6 +32,34 @@ pub fn validate_inputs(public_call: PublicCallData) {
         this_call_stack_item.function_data.is_private == false, "Cannot execute a private function with the public kernel circuit"
     );
     assert(public_call.bytecode_hash != 0, "Bytecode hash cannot be zero");
+}
+
+pub fn validate_public_call_global_variables(public_call: PublicCallData, constants: CombinedConstantData) {
+    let public_call_globals = public_call.call_stack_item.public_inputs.global_variables;
+    assert(
+        public_call_globals == constants.global_variables, "Global variables injected into the public call do not match constants"
+    );
+}
+
+// Validates constants injected into the public call are correct.
+// Note that the previous_kernel.public_inputs.constants returned from the private kernel tail
+// will be empty, so in the first run on of this circuit we load them from the first public
+// call, following the same pattern as in the private_kernel_init.
+// TODO(@spalladino): This can be a security risk since it allows a sequencer to run public
+// circuits with empty global variables. This must be patched by having a differentiated init public
+// circuit that runs only once, or by having a way to differentiate when we're coming from a private 
+// kernel tail vs from another public run.
+pub fn initialize_from_or_validate_public_call_variables(
+    previous_kernel: PublicKernelData,
+    public_call: PublicCallData,
+    public_inputs: &mut PublicKernelCircuitPublicInputsBuilder
+) {
+    if public_inputs.constants.global_variables.is_empty() {
+        let public_call_global_variables = public_call.call_stack_item.public_inputs.global_variables;
+        public_inputs.constants.global_variables = public_call_global_variables;
+    } else {
+        validate_public_call_global_variables(public_call, previous_kernel.public_inputs.constants);
+    }
 }
 
 pub fn validate_public_call_non_revert(public_call: PublicCallData) {

noir-projects/noir-protocol-circuits/crates/public-kernel-lib/src/public_kernel_app_logic.nr
@@ -30,6 +30,9 @@ impl PublicKernelAppLogicCircuitPrivateInputs {
         // validate the inputs common to all invocation circumstances
         common::validate_inputs(self.public_call);
 
+        // validate constants injected into the public call are correct or set them if this is the first public call
+        common::initialize_from_or_validate_public_call_variables(self.previous_kernel, self.public_call, &mut public_inputs);
+
         // validate the inputs unique to having a previous public kernel
         self.validate_inputs();
 
@@ -482,6 +485,27 @@ mod tests {
         let mut builder = PublicKernelAppLogicCircuitPrivateInputsBuilder::new();
 
         builder.public_call.public_inputs.transaction_fee = 10;
+
+        builder.failed();
+    }
+
+    #[test]
+    fn propagates_global_variables_if_empty() {
+        let mut builder = PublicKernelAppLogicCircuitPrivateInputsBuilder::new();
+
+        builder.public_call.public_inputs.global_variables.block_number = 11;
+
+        let public_inputs = builder.execute();
+
+        assert_eq(public_inputs.constants.global_variables.block_number, 11);
+    }
+
+    #[test(should_fail_with="Global variables injected into the public call do not match constants")]
+    fn validates_global_variables() {
+        let mut builder = PublicKernelAppLogicCircuitPrivateInputsBuilder::new();
+
+        builder.previous_kernel.global_variables.block_number = 10;
+        builder.public_call.public_inputs.global_variables.block_number = 11;
 
         builder.failed();
     }

noir-projects/noir-protocol-circuits/crates/public-kernel-lib/src/public_kernel_setup.nr
@@ -8,7 +8,8 @@ struct PublicKernelSetupCircuitPrivateInputs {
     // Note: One might think that our previous_kernel ought to be
     // a PrivateKernelTailData. However, we instead supply a PublicKernelData.
     // This is because PrivateKernelTailData is a subset of PublicKernelData.
-    // And we just initialize the missing values to zero in TS before passing it to the circuit.
+    // And we just initialize the missing values to zero in TS before passing it to the circuit,
+    // except for the constants.global_variables which we populate with the current block values.
     // This is a bit of a hack, but it allows us to reuse the setup circuit until
     // the setup phase of the public kernel is complete. Maybe in a perfect world we would 
     // have a SetupInit, SetupInner, etc, but this will change anyway once the public VM is able to 
@@ -35,6 +36,9 @@ impl PublicKernelSetupCircuitPrivateInputs {
 
         // validate the inputs common to all invocation circumstances
         common::validate_inputs(self.public_call);
+
+        // validate constants injected into the public call are correct or set them if this is the first public call
+        common::initialize_from_or_validate_public_call_variables(self.previous_kernel, self.public_call, &mut public_inputs);
 
         // validate the inputs unique to having a previous private kernel
         self.validate_inputs();
@@ -537,6 +541,27 @@ mod tests {
         let mut builder = PublicKernelSetupCircuitPrivateInputsBuilder::new();
 
         builder.public_call.public_inputs.transaction_fee = 10;
+
+        builder.failed();
+    }
+
+    #[test]
+    fn propagates_global_variables_if_empty() {
+        let mut builder = PublicKernelSetupCircuitPrivateInputsBuilder::new();
+
+        builder.public_call.public_inputs.global_variables.block_number = 11;
+
+        let public_inputs = builder.execute();
+
+        assert_eq(public_inputs.constants.global_variables.block_number, 11);
+    }
+
+    #[test(should_fail_with="Global variables injected into the public call do not match constants")]
+    fn validates_global_variables() {
+        let mut builder = PublicKernelSetupCircuitPrivateInputsBuilder::new();
+
+        builder.previous_kernel.global_variables.block_number = 10;
+        builder.public_call.public_inputs.global_variables.block_number = 11;
 
         builder.failed();
     }

Forostovec/aztec-packages

commit 07fe399e35ab05308e5adb7fc339e0727d92de18

fix: Calculate tx fee using current constants in public kernel (#6066)
Fixes a bug introduced in
https://github.com/AztecProtocol/aztec-packages/pull/6031. If the tx
_only_ has a teardown public function, then the global variables will
still be empty on the previous-kernel of the public kernel teardown
(since it's actually a private-kernel-tail), so the block fees will be
empty.

noir-projects/noir-protocol-circuits/crates/public-kernel-lib/src/public_kernel_teardown.nr
@@ -32,11 +32,11 @@ impl PublicKernelTeardownCircuitPrivateInputs {
     }
 
     // Validates the transaction fee injected into the app circuit is properly computed from gas_used and block gas_fees
-    fn validate_transaction_fee(self) {
+    fn validate_transaction_fee(self, public_inputs: PublicKernelCircuitPublicInputsBuilder) {
         let transaction_fee = self.public_call.call_stack_item.public_inputs.transaction_fee;
         // Note that teardown_gas is already included in end.gas_used as it was injected by the private kernel
         let total_gas_used = self.previous_kernel.public_inputs.end.gas_used.add(self.previous_kernel.public_inputs.end_non_revertible.gas_used);
-        let block_gas_fees = self.previous_kernel.public_inputs.constants.global_variables.gas_fees;
+        let block_gas_fees = public_inputs.constants.global_variables.gas_fees;
         let inclusion_fee = self.previous_kernel.public_inputs.constants.tx_context.gas_settings.inclusion_fee;
         let computed_transaction_fee = total_gas_used.compute_fee(block_gas_fees) + inclusion_fee;
 
@@ -84,7 +84,7 @@ impl PublicKernelTeardownCircuitPrivateInputs {
         common::validate_call_against_request(self.public_call, call_request);
 
         self.validate_start_gas();
-        self.validate_transaction_fee();
+        self.validate_transaction_fee(public_inputs);
 
         common::update_validation_requests(self.public_call, &mut public_inputs);
 

yarn-project/sequencer-client/src/global_variable_builder/global_builder.ts
@@ -86,11 +86,17 @@ export class SimpleTestGlobalVariableBuilder implements GlobalVariableBuilder {
       lastTimestamp = new Fr(lastTimestamp.value + 1n);
     }
 
-    this.log.debug(
-      `Built global variables for block ${blockNumber}: (${chainId}, ${version}, ${blockNumber}, ${lastTimestamp}, ${coinbase}, ${feeRecipient})`,
-    );
-
     const gasFees = GasFees.default();
+    const globalVariables = new GlobalVariables(
+      chainId,
+      version,
+      blockNumber,
+      lastTimestamp,
+      coinbase,
+      feeRecipient,
+      gasFees,
+    );
+    this.log.debug(`Built global variables for block ${blockNumber}`, globalVariables.toJSON());
     return new GlobalVariables(chainId, version, blockNumber, lastTimestamp, coinbase, feeRecipient, gasFees);
   }
 }

yarn-project/end-to-end/src/benchmarks/bench_tx_size_fees.test.ts
@@ -61,12 +61,12 @@ describe('benchmarks/tx_size_fees', () => {
     await token.methods.mint_public(aliceWallet.getAddress(), 100e9).send().wait();
   });
 
-  it.each<() => Promise<FeePaymentMethod | undefined>>([
-    () => Promise.resolve(undefined),
-    () => NativeFeePaymentMethod.create(aliceWallet),
-    () => Promise.resolve(new PublicFeePaymentMethod(token.address, fpc.address, aliceWallet)),
-    () => Promise.resolve(new PrivateFeePaymentMethod(token.address, fpc.address, aliceWallet)),
-  ])('sends a tx with a fee', async createPaymentMethod => {
+  it.each<[string, () => Promise<FeePaymentMethod | undefined>]>([
+    ['no', () => Promise.resolve(undefined)],
+    ['native fee', () => NativeFeePaymentMethod.create(aliceWallet)],
+    ['public fee', () => Promise.resolve(new PublicFeePaymentMethod(token.address, fpc.address, aliceWallet))],
+    ['private fee', () => Promise.resolve(new PrivateFeePaymentMethod(token.address, fpc.address, aliceWallet))],
+  ] as const)('sends a tx with a fee with %s payment method', async (_name, createPaymentMethod) => {
     const paymentMethod = await createPaymentMethod();
     const gasSettings = GasSettings.default();
     const tx = await token.methods